3d5882f5ea303a49235a721cfed96f1cf0bd466d
Sara Ngo <sara.ngoo.9641@gmail.com>
2022-01-22 12:23:45 -0800

    add email field to form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+
+from .forms import SignUpForm
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST) # using self-create form instead of lib form because it doesn't have email field
+        if form.is_valid():
+            user = form.save() # create a user instance
+            auth_login(request, user) # manually authenticating the user
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
     return render(request,'signup.html', {'form':form})
